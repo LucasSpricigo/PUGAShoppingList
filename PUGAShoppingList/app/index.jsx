a140927e101354b3461bf3e917c5669c04e77a52
LucasSpricigo <lucasspricigo21@gmail.com>
2026-05-04 08:51:51 -0300

    Desenvolvido a Tela Inicial

--- PUGAShoppingList/app/index.jsx
+++ PUGAShoppingList/app/index.jsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Link} from 'expo-router'
 
-export default function App() {
+export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerImage}>
+        <Image source={require('../assets/avatar.png')} style={styles.avatar}/>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>PUGA Shopping List</Text>
+        <Text style={styles.text}>
+          Monte sua lista de compras
+        </Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  containerImage: {
+    flex:2,
+    justifyContent: 'center',
+    alignItems: 'centar',
+  },
+avatar: {
+  width: 230,
+  height: 230,
+  borderRadius: 115,
+  resizeMode: 'cover',
+},
+content: {
+  flex:1,
+  marginHorizontal: 5,
+  backgroundColor: '#dadada',
+  borderTopLeftRadius: 25,
+  borderTopRightRadius: 25,
+  paddingHorizontal: '5%'
+},
+title: {
+  fontSize: 30,
+  fontWeight: 'bold',
+  marginVertical: 20,
+  textAling: 'center',
+},
+text: {
+  fontSize: 16,
+  color: 'gray',
+},
+button: {
+  position: 'absolute',
+  backgroundColor: '#000',
+  bottom: '15%',
+  alignSelf: 'center',
+  borderRadius: 50,
+  paddingVertical: 15,
+  width: '60%',
+  textAlign: 'center',
+},
+buttonText: {
+  fontSize: 22,
+  color: '#fff',
+  fontweight:'bold'
+},
 });
