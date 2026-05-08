3f9be706eba44a4f5f8d2eddc7117fb64a2ac56d
LucasSpricigo <lucasspricigo21@gmail.com>
2026-05-08 11:36:35 -0300

    Construida a página Home

--- PUGAShoppingList/app/index.jsx
+++ PUGAShoppingList/app/index.jsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {Link} from 'expo-router'
+import { Link } from 'expo-router'
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
       <View style={styles.containerImage}>
-        <Image source={require('../assets/avatar.png')} style={styles.avatar}/>
+        <Image source={require('../assets/avatar.png')} style={styles.avatar} />
       </View>
       <View style={styles.content}>
         <Text style={styles.title}>PUGA Shopping List</Text>
@@ -26,47 +26,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
   },
   containerImage: {
-    flex:2,
+    flex: 2,
     justifyContent: 'center',
-    alignItems: 'centar',
+    alignItems: 'center',
   },
-avatar: {
-  width: 230,
-  height: 230,
-  borderRadius: 115,
-  resizeMode: 'cover',
-},
-content: {
-  flex:1,
-  marginHorizontal: 5,
-  backgroundColor: '#dadada',
-  borderTopLeftRadius: 25,
-  borderTopRightRadius: 25,
-  paddingHorizontal: '5%'
-},
-title: {
-  fontSize: 30,
-  fontWeight: 'bold',
-  marginVertical: 20,
-  textAling: 'center',
-},
-text: {
-  fontSize: 16,
-  color: 'gray',
-},
-button: {
-  position: 'absolute',
-  backgroundColor: '#000',
-  bottom: '15%',
-  alignSelf: 'center',
-  borderRadius: 50,
-  paddingVertical: 15,
-  width: '60%',
-  textAlign: 'center',
-},
-buttonText: {
-  fontSize: 22,
-  color: '#fff',
-  fontweight:'bold'
-},
+  avatar: {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: 5,
+    backgroundColor: '#dadada',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    textAling: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: '#000',
+    bottom: '15%',
+    alignSelf: 'center',
+    borderRadius: 50,
+    paddingVertical: 15,
+    width: '60%',
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    fontweight: 'bold'
+  },
 });
